16e5e32e404bfcacad7a7e8ca8969363177f3e8b
= <=>
2026-02-12 13:23:34 +0100

    add check fiel

--- ft_check_file.c
+++ ft_check_file.c
@@ -1,20 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   ft_check_file.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldeplace <ldeplace@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/12 13:05:31 by ldeplace          #+#    #+#             */
-/*   Updated: 2026/02/12 13:23:18 by ldeplace         ###   ########.fr       */
+/*   Created: 2026/02/12 13:17:10 by ldeplace          #+#    #+#             */
+/*   Updated: 2026/02/12 13:23:00 by ldeplace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "pipex.h"
 
-# include "libft/libft.h"
+void	ft_check_file(char **argv)
+{
+	int	*i;
 
-void	ft_check_file(char **argv);
-
-#endif
+	i = (int *)malloc(2 8 sizeof(int));
+	if (!i)
+		exit(0);
+	i[0] = open(argv[1]);
+	i[1] = open(argv[1]);
+	if (i[0] == 0)
+		ft_add_file();
+	if (i[1] == 0)
+		ft_add_file();
+	close(i[0]);
+	close(i[1]);
+	free(i);
+}
